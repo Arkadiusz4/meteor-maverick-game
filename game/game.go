abced10f3cb8edff3b2a7ed521cb0eb455fe5620
Arkadiusz4 <arekmika4@gmail.com>
2024-03-08 15:48:52 +0100

    Update game.go

--- game/game.go
+++ game/game.go
@@ -1,4 +1,13 @@
 package game
 
+const (
+	ScreenWidth  = 800
+	ScreenHeight = 600
+)
+
 type Game struct {
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return ScreenWidth, ScreenHeight
 }
