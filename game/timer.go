5a4271b722edef5857fc416870b65adc57e8bf4b
Arkadiusz4 <arekmika4@gmail.com>
2024-03-08 15:26:32 +0100

    Create timer.go

---
+++ game/timer.go
@@ -0,0 +1,32 @@
+package game
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"time"
+)
+
+type Timer struct {
+	currentTicks int
+	targetTicks  int
+}
+
+func NewTimer(d time.Duration) *Timer {
+	return &Timer{
+		currentTicks: 0,
+		targetTicks:  int(d.Milliseconds()) * ebiten.TPS() / 1000,
+	}
+}
+
+func (t *Timer) Update() {
+	if t.currentTicks < t.targetTicks {
+		t.currentTicks++
+	}
+}
+
+func (t *Timer) IsReady() bool {
+	return t.currentTicks >= t.targetTicks
+}
+
+func (t *Timer) Reset() {
+	t.currentTicks = 0
+}
