42275ae0e514d2bebadec471c5000ccc399a67d6
Arkadiusz4 <arekmika4@gmail.com>
2024-03-13 23:19:40 +0100

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -7,24 +7,6 @@ import (
 	_ "image/png"
 )
 
-type Game struct {
-	player *game.Player
-}
-
-func (g *Game) Update() error {
-	g.player.Update()
-
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	g.player.Draw(screen)
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return outsideWidth, outsideHeight
-}
-
 func main() {
 	g := game.NewGame()
 
