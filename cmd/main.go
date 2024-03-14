3e210002249f56fdad3c9022e859d7aff2c644b3
Arkadiusz4 <arekmika4@gmail.com>
2024-03-15 00:09:34 +0100

    Train things to finally work 🙃

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,17 @@ import (
 	"github.com/Arkadiusz4/meteor-maverick-game/game"
 	"github.com/hajimehoshi/ebiten/v2"
 	_ "image/png"
-	"os"
 )
 
 func main() {
-	err := os.Setenv("DISPLAY", ":0")
-	if err != nil {
-		panic(err)
-	}
+	//err := os.Setenv("DISPLAY", ":0")
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	g := game.NewGame()
 
-	err = ebiten.RunGame(g)
+	err := ebiten.RunGame(g)
 	if err != nil {
 		panic(err)
 	}
