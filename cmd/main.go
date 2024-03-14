34fdee60e8268d355001a4637d4347a0db06b46f
Arkadiusz4 <arekmika4@gmail.com>
2024-03-14 23:23:56 +0100

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -5,12 +5,19 @@ import (
 	_ "github.com/Arkadiusz4/meteor-maverick-game/game"
 	"github.com/hajimehoshi/ebiten/v2"
 	_ "image/png"
+	"os"
 )
 
 func main() {
+	err := os.Setenv("DISPLAY", ":0")
+	if err != nil {
+		// Handle error if setting environment variable fails
+		panic(err)
+	}
+
 	g := game.NewGame()
 
-	err := ebiten.RunGame(g)
+	err = ebiten.RunGame(g)
 	if err != nil {
 		panic(err)
 	}
