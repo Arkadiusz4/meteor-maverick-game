e4cbdb3d077053ea9061e96775c03b977f894188
Arkadiusz4 <arekmika4@gmail.com>
2024-03-15 00:48:15 +0100

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -3,22 +3,12 @@ package main
 import (
 	"github.com/Arkadiusz4/meteor-maverick-game/game"
 	"github.com/hajimehoshi/ebiten/v2"
-	_ "github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"os"
 )
 
 func main() {
-	err := os.Setenv("EBITEN_HEADLESS", "1")
-	if err != nil {
-		panic(err)
-	}
-
-	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Headless Mode")
-
 	g := game.NewGame()
 
-	err = ebiten.RunGame(g)
+	err := ebiten.RunGame(g)
 	if err != nil {
 		panic(err)
 	}
